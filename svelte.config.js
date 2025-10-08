7eab79ed167ef53c0c1371c0afa64ae3bdd0bf79
Lucas Levine <lucaslevine12@gmail.com>
2025-10-07 21:17:17 -0400

    adapter fix 2.0

--- svelte.config.js
+++ svelte.config.js
@@ -6,9 +6,9 @@ const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  adapter: adapter(),
 
   kit: {
+    adapter: adapter(),
     experimental: {
       remoteFunctions: true,
     },
